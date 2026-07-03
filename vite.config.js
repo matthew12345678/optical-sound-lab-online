234d424ef8bfe1fed910a983d73c3eda2b6bf1c9
matthew olden <matthewolden@matthews-MacBook-Pro.local>
2026-07-03 12:44:22 +0100

    prepare github pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
   base: "/optical-sound-lab/",
   plugins: [react()]
 })
+
+export default {
+  base: "/optical-sound-lab/",
+}
+
