526a6379a585fb12791e94764bb000cb5d5475e2
matthew olden <matthewolden@matthews-MacBook-Pro.local>
2026-07-04 08:55:02 +0100

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite'
-
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+ 
 export default defineConfig({
   base: "/optical-sound-lab-online/",
+  plugins: [react(), tailwindcss()],
 })
+ 
